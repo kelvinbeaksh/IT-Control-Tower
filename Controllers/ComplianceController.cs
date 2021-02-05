fab37798ff4e639bb00d79caf56c951f9f9d646d
shuwen1999 <pangshuwen99@gmail.com>
2021-02-05 15:58:35 +0800

    updatess

---
+++ Controllers/ComplianceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IT_Control_Tower.Controllers
+{
+    public class ComplianceController : Controller
+    {
+        // GET: Compliance
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/SAMController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IT_Control_Tower.Controllers
+{
+    public class SAMController : Controller
+    {
+        // GET: SAM
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
