adb46e70f45b164bf09d4083e7aeaa870eb3597f
APA\SESA590429 <brightsky16@gmail.com>
2021-06-01 13:57:31 +0800

    feat: SAM Dashboards
    - Embed Software Asset Gap
    - Embed Other Cautionary
    - Embed Multiple Office Edition

--- Controllers/SAMController.cs
+++ Controllers/SAMController.cs
@@ -13,5 +13,25 @@ namespace IT_Control_Tower.Controllers
         {
             return View();
         }
+        public ActionResult softwareAssetTrackerDashboard()
+        {
+            return View();
+        }
+        public ActionResult softwareAssetGapDashboard()
+        {
+            return View();
+        }
+        public ActionResult abobeCautionaryDashboard()
+        {
+            return View();
+        }
+        public ActionResult otherCautionaryDashboard()
+        {
+            return View();
+        }
+        public ActionResult multipleOfficeEditionDashboard()
+        {
+            return View();
+        }
     }
 }
